fe575689d6e84f7c0bd768c9a9d219a249eb9194
william08190 <104564998+william08190@users.noreply.github.com>
2024-12-12 15:39:41 +0800

    Update binance_perpetual_auth.py

--- hummingbot/connector/derivative/binance_perpetual/binance_perpetual_auth.py
+++ hummingbot/connector/derivative/binance_perpetual/binance_perpetual_auth.py
@@ -1,8 +1,7 @@
-import hashlib
+import base64
 import hmac
 import json
-from collections import OrderedDict
-from typing import Any, Dict
+from typing import Any, Dict, List
 from urllib.parse import urlencode
 
 from hummingbot.connector.time_synchronizer import TimeSynchronizer
@@ -10,45 +9,63 @@ from hummingbot.core.web_assistant.auth import AuthBase
 from hummingbot.core.web_assistant.connections.data_types import RESTMethod, RESTRequest, WSRequest
 
 
-class BinancePerpetualAuth(AuthBase):
+class BitgetPerpetualAuth(AuthBase):
     """
-    Auth class required by Binance Perpetual API
+    Bitget Perpetual API 的认证类
     """
-
-    def __init__(self, api_key: str, api_secret: str, time_provider: TimeSynchronizer):
+    def __init__(self, api_key: str, secret_key: str, passphrase: str, time_provider: TimeSynchronizer):
         self._api_key: str = api_key
-        self._api_secret: str = api_secret
+        self._secret_key: str = secret_key
+        self._passphrase: str = passphrase
         self._time_provider: TimeSynchronizer = time_provider
 
-    def generate_signature_from_payload(self, payload: str) -> str:
-        secret = bytes(self._api_secret.encode("utf-8"))
-        signature = hmac.new(secret, payload.encode("utf-8"), hashlib.sha256).hexdigest()
-        return signature
-
     async def rest_authenticate(self, request: RESTRequest) -> RESTRequest:
-        if request.method == RESTMethod.POST:
-            request.data = self.add_auth_to_params(params=json.loads(request.data))
-        else:
-            request.params = self.add_auth_to_params(request.params)
+        headers = {
+            "Content-Type": "application/json",
+            "ACCESS-KEY": self._api_key,
+            "ACCESS-TIMESTAMP": str(int(self._time_provider.time() * 1e3)),
+            "ACCESS-PASSPHRASE": self._passphrase,
+        }
 
-        request.headers = self.header_for_authentication()
+        path = request.throttler_limit_id
+        query_string = urlencode(request.params) if request.params else ""
+        if query_string:
+            path += f"?{query_string}"
 
+        body = json.dumps(request.data) if request.data else ""
+        pre_hash_string = self._pre_hash(headers["ACCESS-TIMESTAMP"], request.method.value, path, body)
+        headers["ACCESS-SIGN"] = self._sign(pre_hash_string, self._secret_key)
+
+        request.headers.update(headers)
         return request
 
     async def ws_authenticate(self, request: WSRequest) -> WSRequest:
-        return request  # pass-through
+        """
+        配置 WebSocket 请求以进行身份验证
+        """
+        return request  # 直接返回
 
-    def add_auth_to_params(self,
-                           params: Dict[str, Any]):
-        timestamp = int(self._time_provider.time() * 1e3)
+    def get_ws_auth_payload(self) -> List[Dict[str, Any]]:
+        """
+        生成用于 WebSocket 认证的负载
+        :return: 包含认证信息的字典列表
+        """
+        timestamp = str(int(self._time_provider.time() * 1e3))
+        pre_hash_string = self._pre_hash(timestamp, "GET", "/user/verify", "")
+        signature = self._sign(pre_hash_string, self._secret_key)
+        auth_info = [{
+            "apiKey": self._api_key,
+            "passphrase": self._passphrase,
+            "timestamp": timestamp,
+            "sign": signature,
+        }]
+        return auth_info
 
-        request_params = OrderedDict(params or {})
-        request_params["timestamp"] = timestamp
+    @staticmethod
+    def _sign(message: str, secret_key: str) -> str:
+        mac = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), digestmod='sha256')
+        return base64.b64encode(mac.digest()).decode().strip()
 
-        payload = urlencode(request_params)
-        request_params["signature"] = self.generate_signature_from_payload(payload=payload)
-
-        return request_params
-
-    def header_for_authentication(self) -> Dict[str, str]:
-        return {"X-MBX-APIKEY": self._api_key}
+    @staticmethod
+    def _pre_hash(timestamp: str, method: str, request_path: str, body: str) -> str:
+        return f"{timestamp}{method.upper()}{request_path}{body}"
