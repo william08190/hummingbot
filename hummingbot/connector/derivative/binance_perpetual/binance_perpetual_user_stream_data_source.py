19a360309accb899078b61bf2bf3b0f0682507c5
william08190 <104564998+william08190@users.noreply.github.com>
2024-12-12 15:42:30 +0800

    Update binance_perpetual_user_stream_data_source.py

--- hummingbot/connector/derivative/binance_perpetual/binance_perpetual_user_stream_data_source.py
+++ hummingbot/connector/derivative/binance_perpetual/binance_perpetual_user_stream_data_source.py
@@ -1,144 +1,128 @@
 import asyncio
-import time
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 
-import hummingbot.connector.derivative.binance_perpetual.binance_perpetual_constants as CONSTANTS
-import hummingbot.connector.derivative.binance_perpetual.binance_perpetual_web_utils as web_utils
-from hummingbot.connector.derivative.binance_perpetual.binance_perpetual_auth import BinancePerpetualAuth
+from hummingbot.connector.derivative.bitget_perpetual import bitget_perpetual_constants as CONSTANTS
+from hummingbot.connector.derivative.bitget_perpetual.bitget_perpetual_auth import BitgetPerpetualAuth
 from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
-from hummingbot.core.utils.async_utils import safe_ensure_future
-from hummingbot.core.web_assistant.connections.data_types import RESTMethod
+from hummingbot.core.web_assistant.connections.data_types import WSJSONRequest, WSPlainTextRequest, WSResponse
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 from hummingbot.core.web_assistant.ws_assistant import WSAssistant
 from hummingbot.logger import HummingbotLogger
 
 if TYPE_CHECKING:
-    from hummingbot.connector.derivative.binance_perpetual.binance_perpetual_derivative import (
-        BinancePerpetualDerivative,
-    )
+    from hummingbot.connector.derivative.bitget_perpetual.bitget_perpetual_derivative import BitgetPerpetualDerivative
 
 
-class BinancePerpetualUserStreamDataSource(UserStreamTrackerDataSource):
-    LISTEN_KEY_KEEP_ALIVE_INTERVAL = 1800  # Recommended to Ping/Update listen key to keep connection alive
-    HEARTBEAT_TIME_INTERVAL = 30.0
+class BitgetPerpetualUserStreamDataSource(UserStreamTrackerDataSource):
     _logger: Optional[HummingbotLogger] = None
 
     def __init__(
-            self,
-            auth: BinancePerpetualAuth,
-            connector: 'BinancePerpetualDerivative',
-            api_factory: WebAssistantsFactory,
-            domain: str = CONSTANTS.DOMAIN,
+        self,
+        auth: BitgetPerpetualAuth,
+        trading_pairs: List[str],
+        connector: 'BitgetPerpetualDerivative',
+        api_factory: WebAssistantsFactory,
+        domain: str = None,
     ):
-
         super().__init__()
         self._domain = domain
         self._api_factory = api_factory
         self._auth = auth
-        self._ws_assistants: List[WSAssistant] = []
+        self._trading_pairs = trading_pairs
         self._connector = connector
-        self._current_listen_key = None
-        self._listen_for_user_stream_task = None
-        self._last_listen_key_ping_ts = None
+        self._pong_response_event = None
 
-        self._manage_listen_key_task = None
-        self._listen_key_initialized_event: asyncio.Event = asyncio.Event()
+    async def _authenticate(self, ws: WSAssistant):
+        """
+        Authenticates user to the WebSocket.
+        """
+        auth_payload: List[Dict[str, Any]] = self._auth.get_ws_auth_payload()
+        payload = {"op": "login", "args": auth_payload}
+        login_request: WSJSONRequest = WSJSONRequest(payload=payload)
+        await ws.send(login_request)
+        response: WSResponse = await ws.receive()
+        message = response.data
 
-    @property
-    def last_recv_time(self) -> float:
-        if self._ws_assistant:
-            return self._ws_assistant.last_recv_time
-        return 0
+        if message.get("event") != "login" or message.get("code") != "00000":
+            self.logger().error("Error authenticating the private WebSocket connection")
+            raise IOError("Private WebSocket connection authentication failed")
 
-    async def _get_ws_assistant(self) -> WSAssistant:
-        if self._ws_assistant is None:
-            self._ws_assistant = await self._api_factory.get_ws_assistant()
-        return self._ws_assistant
-
-    async def _get_listen_key(self):
-        rest_assistant = await self._api_factory.get_rest_assistant()
-        try:
-            data = await rest_assistant.execute_request(
-                url=web_utils.private_rest_url(path_url=CONSTANTS.BINANCE_USER_STREAM_ENDPOINT, domain=self._domain),
-                method=RESTMethod.POST,
-                throttler_limit_id=CONSTANTS.BINANCE_USER_STREAM_ENDPOINT,
-                headers=self._auth.header_for_authentication()
-            )
-        except asyncio.CancelledError:
-            raise
-        except Exception as exception:
-            raise IOError(f"Error fetching user stream listen key. Error: {exception}")
-
-        return data["listenKey"]
-
-    async def _ping_listen_key(self) -> bool:
-        try:
-            data = await self._connector._api_put(
-                path_url=CONSTANTS.BINANCE_USER_STREAM_ENDPOINT,
-                params={"listenKey": self._current_listen_key},
-                is_auth_required=True,
-                return_err=True)
-            if "code" in data:
-                self.logger().warning(f"Failed to refresh the listen key {self._current_listen_key}: {data}")
-                return False
-
-        except asyncio.CancelledError:
-            raise
-        except Exception as exception:
-            self.logger().warning(f"Failed to refresh the listen key {self._current_listen_key}: {exception}")
-            return False
-
-        return True
-
-    async def _manage_listen_key_task_loop(self):
+    async def _process_websocket_messages(self, websocket_assistant: WSAssistant, queue: asyncio.Queue):
         while True:
             try:
-                now = int(time.time())
-                if self._current_listen_key is None:
-                    self._current_listen_key = await self._get_listen_key()
-                    self._listen_key_initialized_event.set()
-                    self._last_listen_key_ping_ts = int(time.time())
-                    self.logger().info(f"Successfully obtained listen key {self._current_listen_key}")
+                await asyncio.wait_for(
+                    super()._process_websocket_messages(websocket_assistant=websocket_assistant, queue=queue),
+                    timeout=CONSTANTS.SECONDS_TO_WAIT_TO_RECEIVE_MESSAGE)
+            except asyncio.TimeoutError:
+                if self._pong_response_event and not self._pong_response_event.is_set():
+                    # The PONG response for the previous PING request was never received
+                    raise IOError("The user stream channel is unresponsive (pong response not received)")
+                self._pong_response_event = asyncio.Event()
+                await self._send_ping(websocket_assistant=websocket_assistant)
 
-                if now - self._last_listen_key_ping_ts >= self.LISTEN_KEY_KEEP_ALIVE_INTERVAL:
-                    success: bool = await self._ping_listen_key()
-                    if success:
-                        self.logger().info(f"Refreshed listen key {self._current_listen_key}.")
-                        self._last_listen_key_ping_ts = int(time.time())
-                    else:
-                        raise Exception(f"Error occurred renewing listen key {self._current_listen_key}")
-            except Exception as e:
-                self.logger().error(f"Error occurred managing the user stream listen key: {e}")
-                self._current_listen_key = None
-                self._listen_key_initialized_event.clear()
-            finally:
-                await asyncio.sleep(5.0)
+    async def _process_event_message(self, event_message: Dict[str, Any], queue: asyncio.Queue):
+        if event_message == CONSTANTS.WS_PONG_RESPONSE and self._pong_response_event:
+            self._pong_response_event.set()
+        elif "event" in event_message:
+            if event_message["event"] == "error":
+                raise IOError(f"Private channel subscription failed ({event_message})")
+        else:
+            await super()._process_event_message(event_message=event_message, queue=queue)
 
-    async def _connected_websocket_assistant(self) -> WSAssistant:
-        """
-        Creates an instance of WSAssistant connected to the exchange
-        """
-        self._manage_listen_key_task = safe_ensure_future(self._manage_listen_key_task_loop())
-        await self._listen_key_initialized_event.wait()
-
-        ws: WSAssistant = await self._get_ws_assistant()
-        url = f"{web_utils.wss_url(CONSTANTS.PRIVATE_WS_ENDPOINT, self._domain)}/{self._current_listen_key}"
-        await ws.connect(ws_url=url, ping_timeout=self.HEARTBEAT_TIME_INTERVAL)
-        return ws
+    async def _send_ping(self, websocket_assistant: WSAssistant):
+        ping_request = WSPlainTextRequest(payload=CONSTANTS.WS_PING_REQUEST)
+        await websocket_assistant.send(ping_request)
 
     async def _subscribe_channels(self, websocket_assistant: WSAssistant):
-        """
-        Subscribes to the trade events and diff orders events through the provided websocket connection.
+        try:
+            product_types = set([await self._connector.product_type_for_trading_pair(trading_pair=trading_pair)
+                                 for trading_pair in self._trading_pairs])
+            subscription_payloads = []
 
-        Binance does not require any channel subscription.
+            for product_type in product_types:
+                subscription_payloads.append(
+                    {
+                        "instType": product_type.upper(),
+                        "channel": CONSTANTS.WS_SUBSCRIPTION_WALLET_ENDPOINT_NAME,
+                        "instId": "default"
+                    }
+                )
+                subscription_payloads.append(
+                    {
+                        "instType": product_type.upper(),
+                        "channel": CONSTANTS.WS_SUBSCRIPTION_POSITIONS_ENDPOINT_NAME,
+                        "instId": "default"
+                    }
+                )
+                subscription_payloads.append(
+                    {
+                        "instType": product_type.upper(),
+                        "channel": CONSTANTS.WS_SUBSCRIPTION_ORDERS_ENDPOINT_NAME,
+                        "instId": "default"
+                    }
+                )
 
-        :param websocket_assistant: the websocket assistant used to connect to the exchange
-        """
-        pass
+            payload = {
+                "op": "subscribe",
+                "args": subscription_payloads
+            }
+            subscription_request = WSJSONRequest(payload)
 
-    async def _on_user_stream_interruption(self, websocket_assistant: Optional[WSAssistant]):
-        websocket_assistant and await websocket_assistant.disconnect()
-        self._manage_listen_key_task and self._manage_listen_key_task.cancel()
-        self._current_listen_key = None
-        self._listen_key_initialized_event.clear()
-        await self._sleep(5)
+            await websocket_assistant.send(subscription_request)
+
+            self.logger().info("Subscribed to private account, position, and orders channels...")
+        except asyncio.CancelledError:
+            raise
+        except Exception:
+            self.logger().exception(
+                "Unexpected error occurred subscribing to account, position, and orders channels..."
+            )
+            raise
+
+    async def _connected_websocket_assistant(self) -> WSAssistant:
+        ws: WSAssistant = await self._api_factory.get_ws_assistant()
+        await ws.connect(
+            ws_url=CONSTANTS.WSS_URL,
+            message_timeout=CONSTANTS.SECONDS_TO_WAIT_TO_RECEIVE_MESSAGE)
+        await self._authenticate(ws)
+        return ws
